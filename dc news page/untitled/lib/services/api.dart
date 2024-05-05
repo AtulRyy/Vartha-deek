95642368036fbaf416cbd95c68febfce3920a471
Atul Reny <117465987+AtulRy@users.noreply.github.com>
2024-05-05 12:05:03 +0530

    Revert "api link changed"
    This reverts commit 4743b62994e3c0bdbf60ecc2d871e47a82465895.

--- dc news page/untitled/lib/services/api.dart
+++ dc news page/untitled/lib/services/api.dart
@@ -4,8 +4,8 @@ import 'package:http/http.dart' as http;
 import 'package:untitled/models/articles.dart';
 
 class ArticleApi{
-  static const String ArticleUrl = 'http://10.0.2.2:3000/api/data'; //emulator link
-  // static const String ArticleUrl = 'http://13.235.134.22:3000/api/data'; 
+  // static const String ArticleUrl = 'http://10.0.2.2:3000/api/data'; //emulator link
+  static const String ArticleUrl = 'http://13.235.134.22:3000/api/data'; 
 //  static const String ArticleUrl = 'http://localhost:3000/api/data';
   static Future<List<Article>> fetchArticle() async {
     final response = await http.get(Uri.parse(ArticleUrl));
